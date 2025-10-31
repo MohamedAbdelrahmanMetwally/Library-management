dce671d1aa0a1fca4aed9605aa653709e801be92
mohamed <mnafe44@gmail.com>
2025-10-31 08:35:11 +0200

    Updating main

--- main.dart
+++ main.dart
@@ -1,14 +1,22 @@
 import 'Book.dart';
 import 'Librarian.dart';
 import 'Student.dart';
+import 'Library.dart';
+
 void main(List<String> args) {
+  Library library = Library();
   Book book1 = Book("FIRST", "MEDO", 5000);
   Book book2 = Book("SECOND", "NEDO", 6000);
   Book book3 = Book("THIRD", "SEDO", 7000);
+  library.addBook(book1);
+  library.addBook(book2);
+  library.addBook(book3);
   Librarian librarian = Librarian("1", "Mohamed");
   Student student = Student("2", "Mody");
-  librarian.borrowBook(book1);
-  student.borrowBook(book2);
-  print("\\nBook Info Example:");
-  print(book3.displayInfo());
+  library.showAvailableBooks();
+  print("");
+  library.lendBook(book1, librarian);
+  library.lendBook(book2, student);
+  print("");
+  library.showAvailableBooks();
 }
